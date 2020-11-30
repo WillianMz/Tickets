5c9c3bd66f4a1e065c0461f003ef4e3e61efc031
WillianMz <willianmazzorana@hotmail.com>
2020-11-30 10:31:50 -0300

    Criado tabela de ticket

--- ticket-web/src/app/paginas/categoria/categoria.component.ts
+++ ticket-web/src/app/paginas/categoria/categoria.component.ts
@@ -10,36 +10,34 @@ import { CategoriaService } from 'src/app/_servicos/categoria.service';
 })
 export class CategoriaComponent implements OnInit {
 
-  categoria = {} as Categoria;
-  categorias: Categoria[];
+  //categoria = {} as Categoria;
+  //categorias: Categoria[];
+  categorias = [];
 
   constructor(private categoriaService: CategoriaService) { }
 
   ngOnInit(): void {
     this.getCategorias();
+    this.categorias = [
+      {'Id':1, 'Titulo':'Bugs',           'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':2, 'Titulo':'Implementação',  'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':3, 'Titulo':'Novos recursos', 'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':4, 'Titulo':'Atualização',    'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':5, 'Titulo':'Suporte',        'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':6, 'Titulo':'Desenvolvimento','Descricao':'Teste de Descrição de Categoria'},
+      {'Id':7, 'Titulo':'Estoque',        'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':8, 'Titulo':'Financeiro',     'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':9, 'Titulo':'Comercial',      'Descricao':'Teste de Descrição de Categoria'},
+      {'Id':10,'Titulo':'Treinamento',    'Descricao':'Teste de Descrição de Categoria'}
+    ];
   }
 
   getCategorias(){
-    this.categoriaService.getAll().subscribe((categorias: Categoria[]) => {
-      this.categorias = categorias;
-    });
+  //  this.categoriaService.getAll().subscribe((categorias: Categoria[]) => {
+    //  this.categorias = categorias;
+    //});
   }
 
-  
-    /*
-    this.categorias = [
-      {'id':1, 'titulo':'Bugs',           'descricao':'Teste de Descrição de Categoria'},
-      {'id':2, 'titulo':'Implementação',  'descricao':'Teste de Descrição de Categoria'},
-      {'id':3, 'titulo':'Novos recursos', 'descricao':'Teste de Descrição de Categoria'},
-      {'id':4, 'titulo':'Atualização',    'descricao':'Teste de Descrição de Categoria'},
-      {'id':5, 'titulo':'Suporte',        'descricao':'Teste de Descrição de Categoria'},
-      {'id':6, 'titulo':'Desenvolvimento','descricao':'Teste de Descrição de Categoria'},
-      {'id':7, 'titulo':'Estoque',        'descricao':'Teste de Descrição de Categoria'},
-      {'id':8, 'titulo':'Financeiro',     'descricao':'Teste de Descrição de Categoria'},
-      {'id':9, 'titulo':'Comercial',      'descricao':'Teste de Descrição de Categoria'},
-      {'id':10,'titulo':'Treinamento',    'descricao':'Teste de Descrição de Categoria'}
-    ];
-    */
-  
+    
 
 }
--- ticket-web/src/app/paginas/ticket/ticket.component.ts
+++ ticket-web/src/app/paginas/ticket/ticket.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketComponent implements OnInit {
 
+  tickets = []
+
   constructor() { }
 
   ngOnInit(): void {
+  
+    this.tickets = [
+      {'Id':1, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':2, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':3, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':4, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':5, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':6, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':7, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':8, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':9, 'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':10,'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':11,'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'},
+      {'Id':12,'Titulo':'Teste','Descricao':'Descrição teste do ticket xnskjdfgskdjfghksdgjh','Categoria':'Teste','Criador':'Willian','DataAbertura':'30/11/2020','Status':'Aberto','Prioridade':'Normal'}
+    ];
+
   }
 
 }
+
