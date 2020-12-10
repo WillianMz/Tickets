dd84a8026eb111e1fdd8590f602683c1f64bbee6
Willian Mazzorana <willianmazzorana@hotmail.com>
2020-12-10 06:24:24 -0300

    metodo salvar categoria form

--- ticket-web/src/app/_servicos/categoria.service.ts
+++ ticket-web/src/app/_servicos/categoria.service.ts
@@ -1,7 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { Categoria } from '../_modelos/categoria';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type':'application/json'})
+};
+
+const urlAPI = 'https://localhost:5001/api/Categoria';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +22,13 @@ export class CategoriaService{
 
   listarTodas(): Observable<Categoria[]>{
     return this.http.get<Categoria[]>(this.API + '/Listar');
+  }
+  
+  NovaCategoria(categoria): Observable<Categoria>{
+    return this.http.post<Categoria>(urlAPI + '/Adicionar', categoria, httpOptions).pipe(
+      tap((categoria: Categoria) => console.log('Categoria adicionada')),
+      catchError(this.handleError<Categoria>('NovaCategoria'))
+    );
   }
 
   filtarPorNome(nome: string): Observable<Categoria[]>{
@@ -32,6 +46,17 @@ export class CategoriaService{
 
   Editar(categoria: Categoria){
     return this.http.put(this.API + '/Editar', categoria);
+  }
+
+
+
+
+  //interceptar erros caso ocorra
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error:any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    }
   }
 
 }
--- ticket-web/src/app/paginas/categoria/categoria-form/categoria-form.component.ts
+++ ticket-web/src/app/paginas/categoria/categoria-form/categoria-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Categoria } from 'src/app/_modelos/categoria';
 import { CategoriaService } from 'src/app/_servicos/categoria.service';
 import { switchMap} from 'rxjs/operators';
+import { SubjectSubscriber } from 'rxjs/internal/Subject';
 
 @Component({
   selector: 'app-categoria-form',
@@ -18,8 +19,14 @@ export class CategoriaFormComponent implements OnInit {
   acaoAtual: string;//novo/editando
   mensagensDeErro: string[] = null;
   habilitaBotaoSalvar: boolean = false;
+  //retorno: string[];
 
   categoria: Categoria = new Categoria();
+
+  mensagens: string[];
+  mensagem: string;
+  sucesso: boolean;
+  dados: string[];
 
   constructor(    
     private formBuilder: FormBuilder,
@@ -30,13 +37,80 @@ export class CategoriaFormComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.SituacaoAtualDoForm();
-    //this.validacaoDoFormulario();
-    //this.carregarCategoria();
+    this.validarFormulario();
   }
 
-  //metodos publicos
-  submitForm(){
+  validarFormulario(){
+    this.categoriaForm = this.formBuilder.group({
+      id: [null],
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(40)]]
+    });
+  }
+
+  salvar(){
+    //habilita o botao salvar
+    this.habilitaBotaoSalvar = true;
+
+    if(this.acaoAtual == 'Novo')
+      this.novaCategoria();
+    
+  }
+
+  private obterAcaoAtual(){
+    //verifica a ação que esta sendo executada
+    if(this.route.snapshot.url[0].path == 'categorias/novo')
+      this.acaoAtual = 'Novo';
+    else
+      this.acaoAtual == 'Editar';
+  }
+
+  novaCategoria(){
+    this.categoriaService.NovaCategoria(this.categoria).subscribe(
+      (retorno) => {
+        this.sucesso = retorno['sucesso'];
+        this.mensagem = retorno['mensagem'];
+        this.dados = retorno['dados'];
+
+        if(this.sucesso == true){
+          this.toastr.success('Nova categoria adicionada com sucesso!');
+        }
+        else{
+          this.toastr.warning('Categoria não adicionada. Motivo: '+ this.mensagem);
+        }
+      },
+      erro => {
+        this.toastr.error('Ocorreu um erro: ' + erro, 'Nova categoria');
+      }
+    )
+  }
+
+
+
+  salvarCategoriaTeste(){
+    this.categoria.nome = 'ategoria2';
+    this.categoriaService.NovaCategoria(this.categoria).subscribe(
+      (mensagens) => {
+        this.sucesso = mensagens['sucesso'];
+        this.mensagem = mensagens['mensagem'];
+        this.dados = mensagens['dados'];
+        console.log(this.sucesso + this.mensagem);
+        
+        if(this.sucesso == true){
+          this.toastr.success('Categoria adicionada com sucesso');
+        }
+        else{
+          this.toastr.warning('Categoria não adicionada. Motivo: ' + this.mensagem);
+        }
+
+      },
+      erro => {
+        console.log(erro);
+      }
+    )
+  }
+
+    //metodos publicos
+/*   submitForm(){
     //desbloquear o botao ao enviar o formulario
     this.habilitaBotaoSalvar = true;
 
@@ -44,12 +118,22 @@ export class CategoriaFormComponent implements OnInit {
       this.novaCategoria();
     else
       this.editarCategoria();
-  }
+  } */
 
+/*   salvarCategoria(form: NgForm){
+    this.categoriaService.NovaCategoria(form).subscribe(
+      res => {
+        console.log('passou aqui');
+      },(error) => {
+        console.log(error);
+      }
+    );
+  }
+ */
 
   //metodos privados
 
-  private novaCategoria(){
+/*   private novaCategoria(){
     //atribui ao objeto categoria os valores preenchidos no categoriaForm
     const categ: Categoria = Object.assign(new Categoria(), this.categoriaForm.value);
 
@@ -58,8 +142,8 @@ export class CategoriaFormComponent implements OnInit {
       error => this.erroAoGravar(error)
     );
   }
-
-  private editarCategoria(){
+ */
+/*   private editarCategoria(){
     const categ: Categoria = Object.assign(new Categoria(), this.categoriaForm.value);
 
     this.categoriaService.Editar(categ).subscribe(
@@ -67,24 +151,24 @@ export class CategoriaFormComponent implements OnInit {
       error => this.erroAoGravar(error)
     );
   }
+ */
 
-
-  private SituacaoAtualDoForm() {
+/*   private SituacaoAtualDoForm() {
     //verifica a acao que esta sendo executada no momento. Criando nova categoria, editando, etc...
     if(this.route.snapshot.url[0].path == 'Novo')
       this.acaoAtual = 'Adicionando';
     else  
       this.acaoAtual = "Editando";
-  }
+  } */
 
-  private validacaoDoFormulario(){
+/*   private validacaoDoFormulario(){
     this.categoriaForm = this.formBuilder.group({
       id: [null],
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(40)]]
     });
   }
-
-  private carregarCategoria() {
+ */
+/*   private carregarCategoria() {
     if(this.acaoAtual == 'Editando'){
       this.route.paramMap.pipe(
         switchMap(params => this.categoriaService.filtarPorID(+params.get('id')))).subscribe(
@@ -95,23 +179,23 @@ export class CategoriaFormComponent implements OnInit {
           (error) => alert('eero')
         ) 
     }
-  }
+  } */
 
-  private tituloDaPagina() {
+/*   private tituloDaPagina() {
     if(this.acaoAtual == 'Adicionando')
       this.Titulo = 'Nova Categoria';
     else
       this.Titulo = 'Editando Categoria';
-  }
+  } */
 
-  private sucessoAoGravar(categoria: Categoria) {
+ /*  private sucessoAoGravar(categoria: Categoria) {
     this.toastr.success('Dados salvos com sucesso!','Gravar dados');
     
     //redicioa para outra rota e não salva no historico do navegador, não permitindo usar o botao voltar do navegador
     this.router.navigateByUrl('categorias',{ skipLocationChange: true});
   }
-
-  private erroAoGravar(error){
+ */
+/*   private erroAoGravar(error){
     this.toastr.error('Ocorreu um erro ao gravar os dados','Gravar dados');
     this.habilitaBotaoSalvar = false;
 
@@ -120,7 +204,7 @@ export class CategoriaFormComponent implements OnInit {
     else
       this.mensagensDeErro = ['Falha na comunicação com o servidor. Por favor, tente mais tarde'];
       
-  }
+  } */
 
 
 }
