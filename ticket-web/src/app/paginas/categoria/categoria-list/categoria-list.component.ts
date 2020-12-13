c556dfbe1a46c735b1f83984ea1b5d79624ea453
Willian Mazzorana <willianmazzorana@hotmail.com>
2020-12-13 08:16:20 -0300

    Listagem de categorias

--- ticket-web/src/app/paginas/categoria/categoria-list/categoria-list.component.ts
+++ ticket-web/src/app/paginas/categoria/categoria-list/categoria-list.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Categoria } from 'src/app/_modelos/categoria';
 import { CategoriaService } from 'src/app/_servicos/categoria.service';
 import { ToastrService } from 'ngx-toastr';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria-list',
@@ -13,37 +11,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CategoriaListComponent implements OnInit {
   
   Titulo = 'Categoria'
-
-  categoria: Categoria;
   categorias: Categoria[];
   mensagem: string;
-  sucesso: boolean;
-
-  modalRef: BsModalRef;
-  configModal = {
-    backdrop: true,
-    ignoreBackdropClick: true
-  }
-
+  sucesso: boolean;  
 
   constructor(
     private categoriaService: CategoriaService,
-    private toastr: ToastrService,
-    private modalService: BsModalService,
-    private formBuilder: FormBuilder
+    private toastr: ToastrService
   ) { }
 
   ngOnInit(): void {
     this.listarCategorias();
   }
 
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, this.configModal);
-  }
-
-  cancelar(): void{
-    this.modalRef.hide();
-  }
   
   listarCategorias(){
     this.categoriaService.listarTodas().subscribe(
@@ -51,16 +31,17 @@ export class CategoriaListComponent implements OnInit {
         this.sucesso = categorias['sucesso'];
         this.mensagem = categorias['mensagem'];        
         this.categorias = categorias['dados'];
-        this.toastr.success('Ok','Categorias');        
+        this.toastr.success('Ok','Categorias');   
       },
       erro => {
-        //console.log('Erro ao listar categorias. Detalhes: ' + {erro});
-        if(this.categorias == null){
+       /*  if(this.categorias == null){
           this.toastr.warning('Erro ao listar categorias. Sem conexão com o servidor.','Categorias');
-        }
-
-        //this.toastr.error('Erro ao listar categorias. Sem conexão com o servidor.','Categorias');
+        } */
+        this.toastr.error(erro);
       }
+
+      
+
     );
   }
 
